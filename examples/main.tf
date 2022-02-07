dd70d374ca2f2d78713fb34d771e81ed09861730
mima <Michaela.Mattes@t-systems.com>
2022-02-07 10:01:48 +0100

    change variable behaviour

---
+++ examples/main.tf
@@ -0,0 +1,24 @@
+module "network" {
+  source = "../modules/azure/terraform-network"
+  virtual_network = {
+    env = {
+      name                = "service-env-vn"
+      location            = "westeurope"
+      resource_group_name = "service-env-rg"
+      address_space       = ["192.30.100.0/23"]
+      tags = {
+        service = "service_name"
+      }
+    }
+  }
+  subnet = {
+    aks = {
+      name                                           = "service-aks-sub"
+      resource_group_name                            = "service-env-rg"
+      virtual_network_name                           = module.network.virtual_network.env.name
+      address_prefixes                               = ["192.30.100.0/24"]
+      enforce_private_link_endpoint_network_policies = true
+      service_endpoints                              = ["Microsoft.AzureCosmosDB"]
+    }
+  }
+}
